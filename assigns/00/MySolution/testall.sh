f5ef959190e27ad7da81d95ba397e8a921c12080
zhh0907 <123614089+zhh0907@users.noreply.github.com>
2023-01-25 23:59:16 -0500

    Delete testall.sh

--- assigns/00/MySolution/testall.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-sml < assign00-01-test.sml
-sml < assign00-02-test.sml
-sml < assign00-03-test.sml
-sml < assign00-04-test.sml
-sml < assign00-05-test.sml
-
-###### end of [testall.sh] ######
